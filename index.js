19e3d7340956b0309f4d2032113d8d4c0ebf5756
texnikman <azimjon9515@gmail.com>
2022-04-15 23:12:26 +0500

    music bob ketdi bu bot

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Xush kelibsiz'))
+
+bot.start((ctx) => ctx.reply("Assalomu Alaykum aziz foydalanuvchi Siz Elyorbek Temirov Rasmiy guruhiga qo'shildingiz", {
+    "reply_markup": {
+        "resize_keyboard": true,
+        "keyboard": [ // different every time
+            ["Elyorbek Temirov haqda"],
+            ["Qo'shiqlari", "Kliplari"],
+            ["To'ylarda videolari"],
+            ["Bog'lanish"]
+        ]
+    }
+}))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears('salom', (ctx) => ctx.reply('salom qaleysiz'))
-bot.hears('azimjon', (ctx) => ctx.reply('salom Azimjon'))
+bot.hears('bevafo', (ctx) => ctx.replyWithAudio('https://t.me/elyorbekmusic/14111', { caption: 'Elyorbek Temirovning Rasmiy guruhi: https://t.me/elyorbekmusic' }))
 
 bot.launch()
 
